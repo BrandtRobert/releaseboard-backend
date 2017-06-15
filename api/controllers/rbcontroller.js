e502c99e09032366b8e2c9680df590ab28d92d23
breutimann <breutimann@envysion.com>
2017-06-15 14:23:04 -0600

    Init release board backend

---
+++ api/controllers/rbcontroller.js
@@ -0,0 +1,42 @@
+'use strict'
+var mongoose = require('mongoose')
+var Release = mongoose.model('releases')
+
+exports.get_all_releases = function (req, res) {
+  Release.find({}, function (err, release) {
+    if (err) {
+      res.send(err)
+    }
+    res.json(release)
+  })
+}
+
+exports.create_release = function (req, res) {
+  var newRelease = new Release(req.body)
+  newRelease.save(function (err, release) {
+    if (err) {
+      res.send(err)
+    }
+    res.json(release)
+  })
+}
+
+exports.update_release = function (req, res) {
+  Release.findOneAndUpdate(req.params.id, req.body, {new: true}, function (err, release) {
+    if (err) {
+      res.send(err)
+    }
+    res.json(release)
+  })
+}
+
+exports.delete_release = function (req, res) {
+  Release.remove({
+    _id: req.params.id
+  }, function (err, release) {
+    if (err) {
+      res.send(err)
+    }
+    res.json({ message: 'Release deleted' })
+  })
+}
